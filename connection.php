e7162021bd1a78e384752563e5f86abe0ad2a400
PONG13202 <khunnongmai17915@gmail.com>
2026-03-16 23:19:48 +0700

    fix

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@ $host = "nozomi.proxy.rlwy.net";
 $user = "root";
 $pass = "RCzloYIVFpRXDRIAMThtJnJTAsllQDBg";
 $db   = "railway";
-$port = "28785";
+$port = 28785;
 
 $project_connect = mysqli_connect($host,$user,$pass,$db,$port);
 
@@ -17,22 +17,3 @@ if(!$project_connect){
 mysqli_set_charset($project_connect,"utf8");
 date_default_timezone_set('Asia/Bangkok');
 ?>
-
-
-<!-- <?php 
-session_start();
-header("Content-Type:text/html;charset=utf-8");
-
-$project_connect = mysqli_connect("localhost","chanathip","pong1234","ayutthaya");
-
-if(!$project_connect){
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-mysqli_query($project_connect, "SET NAMES 'utf8'");
-mysqli_query($project_connect, "SET character_set_results = 'utf8'");
-mysqli_query($project_connect, "SET character_set_client = 'utf8'");
-mysqli_query($project_connect, "SET character_set_connection = 'utf8'");
-date_default_timezone_set('Asia/Bangkok');
-
-?> -->
