9826e484bd9d5ba073d480920609b8507766ee02
PONG13202 <khunnongmai17915@gmail.com>
2026-03-16 22:59:09 +0700

    deploy

--- connection.php
+++ connection.php
@@ -1,23 +1,20 @@
-<?php 
+<?php
 session_start();
 header("Content-Type:text/html;charset=utf-8");
 
-$project_connect = mysqli_connect(
-"render-db-host",
-"render-user",
-"render-password",
-"render-database"
-);
+$host = "nozomi.proxy.rlwy.net";
+$user = "root";
+$pass = "RCzloYIVFpRXDRIAMThtJnJTAsllQDBg";
+$db   = "railway";
+$port = "28785";
+
+$project_connect = mysqli_connect($host,$user,$pass,$db,$port);
 
 if(!$project_connect){
     die("Connection failed: " . mysqli_connect_error());
 }
 
-mysqli_query($project_connect, "SET NAMES 'utf8'");
-mysqli_query($project_connect, "SET character_set_results = 'utf8'");
-mysqli_query($project_connect, "SET character_set_client = 'utf8'");
-mysqli_query($project_connect, "SET character_set_connection = 'utf8'");
-
+mysqli_set_charset($project_connect,"utf8");
 date_default_timezone_set('Asia/Bangkok');
 ?>
 
